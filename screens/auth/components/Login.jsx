8cb1464da5954b1f42138848138bd091dd84e710
sebastian <sebas99cano@gmail.com>
2023-05-23 22:02:18 -0500

    configuracion para deploy (al menos en android), login funcional y organizado con google, se crea hook para empezar migracion del login

--- screens/auth/components/Login.jsx
+++ screens/auth/components/Login.jsx
@@ -11,49 +11,27 @@ import { AuthContext } from "../../../core/context/AuthContext";
 import { Colors as Colors } from "../../../constants/constants";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
+import { AuthConfig } from "../../../api/firebase/Config";
+import { signInWithGoogle } from "../../../api/firebase/Auth";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
-  const [authState, authDispatch] = useContext(AuthContext);
-  const [token, setToken] = useState("");
-  const [userInfo, setUserInfo] = useState(null);
-  const [userData, setUserData] = useState({ email: "", password: "" });
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId:
-      "941100039124-5l0uci195169bk51jppalkbn7gb6lf1h.apps.googleusercontent.com",
-    iosClientId:
-      "941100039124-26ca5q4c7vtmfi807b0vtiunlqr8h4ht.apps.googleusercontent.com",
-    androidClientId:
-      "941100039124-avl8uopk32nirov1kef0mmc022kit4d4.apps.googleusercontent.com",
-  });
-
-  const navigation = useNavigation();
+  const [, authDispatch] = useContext(AuthContext);
+  const [request, response, promptAsync] = Google.useAuthRequest(
+    {
+      clientId: AuthConfig.webClientId,
+      iosClientId: AuthConfig.iosClientId,
+      androidClientId: AuthConfig.androidClientId,
+    },
+    { authorizationEndpoint: "https://www.googleapis.com/userinfo/v2/me" }
+  );
 
   useEffect(() => {
     if (response?.type === "success") {
-      setToken(response.authentication.accessToken);
-      getUserInfo();
+      signInWithGoogle(response.authentication, authDispatch);
     }
-  }, [response, token]);
-
-  const getUserInfo = async () => {
-    try {
-      const response = await fetch(
-        "https://www.googleapis.com/userinfo/v2/me",
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-
-      const user = await response.json();
-      setUserInfo(user);
-      authDispatch({ type: "LOGIN_SUCCESSFULLY", payload: user });
-      console.log("dispathc", user);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  }, [response]);
 
   const validateLogin = () => {
     authDispatch({
@@ -66,7 +44,6 @@ const Login = () => {
     <View style={styles.container}>
       <Text style={styles.title}>The Frontd3v</Text>
       <View style={styles.inputView}>
-        <Text>{userInfo?.name}</Text>
         <TextInput
           style={styles.inputText}
           placeholder="Email..."
