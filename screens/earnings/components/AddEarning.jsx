49ef24cde0f7a97641705583dfebb2721e2bb0b5
sebastian <sebas99cano@gmail.com>
2023-05-26 18:20:44 -0500

    add earning page

---
+++ screens/earnings/components/AddEarning.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+const AddEarning = () => {
+  return (
+    <View>
+      <Text>Form Create Earning</Text>
+    </View>
+  );
+};
+
+export default AddEarning;
